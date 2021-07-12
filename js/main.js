1d19745f1481a26bc4fe7471a6b5e9151655d0d1
SAID BIO WEDE <saidbiowede@gmail.com>
2021-07-12 07:55:17 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var map = L.map('mapid').setView([-25.2744, 133.7751], 4);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-25.2744, 133.7751]).addTo(map)
+    .bindPopup('This is project 001')
+    .openPopup();
+L.marker([-24.3071351, 116.9106403]).addTo(map)
+    .bindPopup('This is project 002')
+    .openPopup();
+    L.marker([-31.1028991, 147.0803318]).addTo(map)
+    .bindPopup('This is project 003')
+    .openPopup();
